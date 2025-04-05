d76f4ad887f359a71abeb307bd5d1bf85f33b41d
Mbogo-anonymous <mbogoalex3@gmail.com>
2025-04-05 23:31:48 +0300

    news addition screen - news heading section

--- app/src/main/java/com/admin/ligiopen/ui/screens/news/newsManagement/NewsAdditionScreen.kt
+++ app/src/main/java/com/admin/ligiopen/ui/screens/news/newsManagement/NewsAdditionScreen.kt
@@ -1,5 +1,8 @@
 package com.admin.ligiopen.ui.screens.news.newsManagement
 
+import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,24 +19,32 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.admin.ligiopen.R
 import com.admin.ligiopen.data.network.models.club.ClubData
 import com.admin.ligiopen.data.network.models.club.clubs
+import com.admin.ligiopen.ui.screens.match.clubs.ClubLogoUpload
 import com.admin.ligiopen.ui.theme.LigiopenadminTheme
 import com.admin.ligiopen.utils.screenFontSize
 import com.admin.ligiopen.utils.screenHeight
@@ -50,6 +61,9 @@ fun NewsAdditionScreenComposable() {
             selectedClubIds = listOf(1, 2, 5),
             onSelectClub = {},
             onRemoveClub = {},
+            coverPhoto = null,
+            onUploadCoverPhoto = {},
+            onRemoveCoverPhoto = {},
             newsAdditionScreenStage = NewsAdditionScreenStage.CLUB_SELECTION_SECTION
         )
     }
@@ -61,6 +75,9 @@ fun NewsAdditionScreen(
     selectedClubIds: List<Int>,
     onSelectClub: (clubId: Int) -> Unit,
     onRemoveClub: (clubId: Int) -> Unit,
+    coverPhoto: Uri?,
+    onUploadCoverPhoto: () -> Unit,
+    onRemoveCoverPhoto: () -> Unit,
     newsAdditionScreenStage: NewsAdditionScreenStage,
     modifier: Modifier = Modifier
 ) {
@@ -74,21 +91,27 @@ fun NewsAdditionScreen(
     ) {
         when(newsAdditionScreenStage) {
             NewsAdditionScreenStage.CLUB_SELECTION_SECTION -> {
-                ClubSelectionScreen(
+                ClubSelectionSectionScreen(
                     selectedClubIds = selectedClubIds,
                     onSelectClub = onSelectClub,
                     onRemoveClub = onRemoveClub,
                     clubs = clubs
                 )
             }
-            NewsAdditionScreenStage.HEADING_SECTION -> {}
+            NewsAdditionScreenStage.HEADING_SECTION -> {
+                NewsHeadingSectionScreen(
+                    coverPhoto = coverPhoto,
+                    onUploadCoverPhoto = onUploadCoverPhoto,
+                    onRemoveCoverPhoto = onRemoveCoverPhoto
+                )
+            }
             NewsAdditionScreenStage.ITEMS_SECTION -> {}
         }
     }
 }
 
 @Composable
-fun ClubSelectionScreen(
+fun ClubSelectionSectionScreen(
     clubs: List<ClubData>,
     selectedClubIds: List<Int>,
     onSelectClub: (clubId: Int) -> Unit,
@@ -189,6 +212,179 @@ fun SelectableClubCard(
     }
 }
 
+@Composable
+fun NewsHeadingSectionScreen(
+    coverPhoto: Uri?,
+    onUploadCoverPhoto: () -> Unit,
+    onRemoveCoverPhoto: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Column {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Previous screen"
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(screenWidth(x = 4.0)))
+                    Text(
+                        text = "News",
+                        fontSize = screenFontSize(x = 16.0).sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Spacer(modifier = Modifier.height(screenHeight(x = 16.0)))
+                Text(
+                    text = "News heading",
+                    fontSize = screenFontSize(x = 14.0).sp
+                )
+                Spacer(modifier = Modifier.height(screenHeight(x = 8.0)))
+                TextField(
+                    label = {
+                        Text(
+                            text = "Title",
+                            fontSize = screenFontSize(x = 14.0).sp
+                        )
+                    },
+                    value = "",
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(screenHeight(x = 8.0)))
+                TextField(
+                    label = {
+                        Text(
+                            text = "Subtitle",
+                            fontSize = screenFontSize(x = 14.0).sp
+                        )
+                    },
+                    value = "",
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(screenHeight(x = 16.0)))
+                Text(
+                    text = "Upload cover photo",
+                    fontSize = screenFontSize(x = 14.0).sp
+                )
+                Spacer(modifier = Modifier.height(screenHeight(x = 16.0)))
+                if(coverPhoto != null) {
+                    Box {
+                        AsyncImage(
+                            model = ImageRequest.Builder(context = LocalContext.current)
+                                .data(coverPhoto)
+                                .crossfade(true)
+                                .build(),
+                            placeholder = painterResource(id = R.drawable.loading_img),
+                            error = painterResource(id = R.drawable.ic_broken_image),
+                            contentScale = ContentScale.Crop,
+                            contentDescription = "Cover photo",
+                            modifier = Modifier
+                                .height(screenHeight(x = 250.0))
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(screenWidth(x = 10.0)))
+                        )
+                        IconButton(
+                            modifier = Modifier
+                                .alpha(0.5f)
+                                .background(Color.Black)
+                                .padding(
+                                    start = screenWidth(x = 5.0),
+                                    end = screenWidth(x = 5.0),
+                                )
+                                .align(Alignment.TopEnd),
+                            onClick = onRemoveCoverPhoto
+                        ) {
+                            Icon(
+                                tint = Color.White,
+                                imageVector = Icons.Default.Clear,
+                                contentDescription = "Remove front id"
+                            )
+                        }
+                    }
+                } else {
+                    CoverPhotoUpload(
+                        onUploadPhoto = onUploadCoverPhoto
+                    )
+                }
+
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Publish")
+                Spacer(modifier = Modifier.width(screenWidth(x = 4.0)))
+                Icon(
+                    painter = painterResource(id = R.drawable.save),
+                    contentDescription = "Publish news"
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun CoverPhotoUpload(
+    onUploadPhoto: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .height(screenHeight(x = 250.0))
+                .fillMaxWidth()
+                .border(
+                    width = screenWidth(x = 1.0),
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(screenWidth(x = 10.0))
+                )
+        ) {
+            IconButton(
+//                enabled = verificationStatus != VerificationStatus.PENDING_VERIFICATION && verificationStatus != VerificationStatus.VERIFIED,
+                onClick = onUploadPhoto
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.photo_upload),
+                    contentDescription = "Upload photo"
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun NewsAdditionScreenPreview() {
@@ -198,7 +394,10 @@ fun NewsAdditionScreenPreview() {
             selectedClubIds = listOf(1, 2, 5),
             onSelectClub = {},
             onRemoveClub = {},
-            newsAdditionScreenStage = NewsAdditionScreenStage.CLUB_SELECTION_SECTION
+            coverPhoto = null,
+            onUploadCoverPhoto = {},
+            onRemoveCoverPhoto = {},
+            newsAdditionScreenStage = NewsAdditionScreenStage.HEADING_SECTION
         )
     }
 }
